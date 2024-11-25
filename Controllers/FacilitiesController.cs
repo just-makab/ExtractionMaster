b49a858d6c4effffb90b0293021aff959a773d72
just-makab <lwethusibisi@gmail.com>
2024-11-25 11:48:54 +0200

    updated frontend for registration

---
+++ Controllers/FacilitiesController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EM_WebApp.Models;
+using EM_WebApp.Utilities;
+
+namespace EM_WebApp.Controllers
+{
+    public class FacilitiesController : Controller
+    {
+        private readonly EMDbContext _context;
+        private readonly IInputSanitizer _inputSanitizer;
+
+        public FacilitiesController(EMDbContext context, IInputSanitizer inputSanitizer)
+        {
+            _context = context;
+            _inputSanitizer = inputSanitizer;
+
+        }
+
+        // GET: Facilities
+        public async Task<IActionResult> Index()
+        {
+            var eMDbContext = _context.Facilities.Include(f => f.Customer);
+            return View(await eMDbContext.ToListAsync());
+        }
+
+        // GET: Facilities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities
+                .Include(f => f.Customer)
+                .FirstOrDefaultAsync(m => m.FacilityId == id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+
+            return View(facility);
+        }
+
+        // GET: Facilities/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId");
+            return View();
+        }
+
+        // POST: Facilities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FacilityId,CustomerId,Name,Address,PhoneNumber,OperatingHours,IsDeleted")] Facility facility)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(facility);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", facility.CustomerId);
+            return View(facility);
+        }
+
+        // GET: Facilities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities.FindAsync(id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", facility.CustomerId);
+            return View(facility);
+        }
+
+        // POST: Facilities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FacilityId,CustomerId,Name,Address,PhoneNumber,OperatingHours,IsDeleted")] Facility facility)
+        {
+            if (id != facility.FacilityId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(facility);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacilityExists(facility.FacilityId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", facility.CustomerId);
+            return View(facility);
+        }
+
+        // GET: Facilities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var facility = await _context.Facilities
+                .Include(f => f.Customer)
+                .FirstOrDefaultAsync(m => m.FacilityId == id);
+            if (facility == null)
+            {
+                return NotFound();
+            }
+
+            return View(facility);
+        }
+
+        // POST: Facilities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var facility = await _context.Facilities.FindAsync(id);
+            if (facility != null)
+            {
+                _context.Facilities.Remove(facility);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacilityExists(int id)
+        {
+            return _context.Facilities.Any(e => e.FacilityId == id);
+        }
+    }
+}
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,20 +1,24 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace EM_WebApp.ViewModels
 {
-    public class RegisterViewModel
+    public class RegisterViewModel : IValidatableObject
     {
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
         [Required]
         [DisplayName("First Name")]
         public string FirstName { get; set; }
+
         [Required]
         [DisplayName("Last Name")]
         public string LastName { get; set; }
+
         [Required]
         [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
@@ -29,5 +33,26 @@ namespace EM_WebApp.ViewModels
         [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            // Phone number validation: must include a country code
+            if (!Regex.IsMatch(PhoneNumber, @"^\+\d{1,3}\d{6,14}$"))
+            {
+                yield return new ValidationResult(
+                    "Phone number must include a valid country code (e.g., +123456789).",
+                    new[] { nameof(PhoneNumber) }
+                );
+            }
+
+            // Password strength validation
+            if (!Regex.IsMatch(Password, @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$"))
+            {
+                yield return new ValidationResult(
+                    "Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, a number, and a special character.",
+                    new[] { nameof(Password) }
+                );
+            }
+        }
     }
 }
